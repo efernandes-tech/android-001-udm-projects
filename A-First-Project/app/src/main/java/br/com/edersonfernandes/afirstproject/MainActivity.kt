bf97381edfe10f7482608c149865656fa71f1b99
Éderson Fernandes <efernandes.tech@gmail.com>
2025-02-13 23:35:01 -0300

    Sorter app

--- A-First-Project/app/src/main/java/br/com/edersonfernandes/afirstproject/MainActivity.kt
+++ A-First-Project/app/src/main/java/br/com/edersonfernandes/afirstproject/MainActivity.kt
@@ -2,10 +2,12 @@ package br.com.edersonfernandes.afirstproject
 
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,7 +21,9 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun click(view: View) {
-        println("Clicked")
+    fun sortNumber(view: View) {
+        val textResult = findViewById<TextView>(R.id.text_result)
+        val number = Random().nextInt(10) + 1
+        textResult.setText("number: $number")
     }
 }
