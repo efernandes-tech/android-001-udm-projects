10237df08aa6c698153c761e5698e5e957e5d93c
Éderson Fernandes <efernandes.tech@gmail.com>
2025-03-30 19:04:42 -0300

    Finished app daily phrases

--- B-Daily-Phrases/app/src/main/java/br/com/edersonfernandes/bdailyphrases/MainActivity.java
+++ B-Daily-Phrases/app/src/main/java/br/com/edersonfernandes/bdailyphrases/MainActivity.java
@@ -1,6 +1,8 @@
 package br.com.edersonfernandes.bdailyphrases;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,15 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
+
+    String[] phrases = {
+        "Phrase 01",
+        "Phrase 02",
+        "Phrase 03",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,4 +31,25 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
     }
+
+    public void generatePhrase(View view) {
+        TextView text = findViewById(R.id.textResult);
+
+        int randomNumber = new Random().nextInt(3);
+
+        text.setText(phrases[randomNumber]);
+    }
+
+    public void showAll(View view) {
+        TextView text = findViewById(R.id.textResult);
+
+        String resultText = "";
+
+        for (String phrase : phrases) {
+            resultText = resultText + phrase + "\n";
+        }
+
+        text.setText(resultText);
+    }
+
 }
